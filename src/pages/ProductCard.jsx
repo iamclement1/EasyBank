17f87938e1c3e0d3ad30fa38c484a907e88b86fe
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-04-08 23:58:49 +0100

    import card icon

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import OnlineBanking from '../assets/icon-online.svg'
+import IconOnboarding from '../assets/icon-onboarding.svg'
+import IconApi from '../assets/icon-api.svg'
+import IconBudgeting from '../assets/icon-budgeting.svg'
 
 function ProductCard() {
 
     return (
     <div>
         <section>
-            <div className="product-card flex md:px-12 justify-between">
-                <div>Card1</div>
+            <div className="product-card md:flex block md:px-24 justify-between
+            md:text-left text-center">
+                <div className='border border-green-300'>
+                    <img src={ OnlineBanking } alt="banking icon" 
+                    classname=""/>
+                    <h2 className='pt-4 md:text-2xl text-gray-600'>Online Banking</h2>
+                </div>
                 <div className="card2">card2</div>
                 <div>Card4</div>
                 <div>Card4</div>
